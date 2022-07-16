293712399b5900ad6aa654effe6d89e94749c783
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-07-16 13:57:48 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,71 +1,75 @@
 #include <bits/stdc++.h>
-//#define int long long
-#define pa pair<int, int>
+#define int long long
+#define pii pair<int, int>
 #define tp tuple<int, int, int>
 #define db(x) cout << x << " "
+#define mp(x, y) make_pair(x, y)
+#define F first
+#define S second
 
 using namespace std;
 __attribute__((optimize("-O3")))
 
-int n;
-int arr[(int)1e7+1];
-int temp_arr[(int)1e7+1];
-int arr2[(int)1e7+1];
-int ps[(int)1e7+2];
-int maxi;
+vector<pii> graph[8+1];
+int point[8+1];
+bool visited[8+1];
+int path[8+1][8+1];
+
+void init();
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> n;
-
-    for (int i = 1; i <= n; i++) {
-        cin >> arr[i];
-    }
-    for (int i = 1; i <= n; i++) {
-        cin >> arr2[i];
-    }
-    for (int i = 1; i <= n; i++) {
-        ps[i] = ps[i-1] + arr[i]*arr2[i];
-    }
-    maxi = ps[n];
-    for (int i = 2; i <= n; i++) {
-        copy(arr, arr+n+1, temp_arr);
-        for (int j = 1; j <= i-1 && i+j <= n; j++) {
-            int sum = 0;
-            swap(temp_arr[abs(i-j)], temp_arr[i+j]);
-            sum += ps[i-j-1]-ps[0];
-            sum += ps[n]-ps[i+j];
-            int l = abs(i-j);
-            int r = i+j;
-            while (l < r) {
-                sum += temp_arr[l]*arr2[l];
-                sum += temp_arr[r]*arr2[r];
-                l++;
-                r--;
+    init();
+    memset(&point, (int) 1e18, sizeof(point));
+    priority_queue<int, vector<int>, greater<int> > pq;
+    pq.push(1);
+    point[1] = 0;
+    while (!pq.empty()) {
+        int top = pq.top();
+        for (auto a : graph[top]) {
+            if (top == 1) {
+                point[a.F] = a.S;
+                pq.push(a.F);
+                continue;
             }
-            sum += temp_arr[l]*arr2[l];
-            maxi = max(maxi, sum);
-        }
-        copy(arr, arr+n+1, temp_arr);
-        for (int j = 1; j <= i-1 && i+j-1 <= n; j++) {
-            int sum = 0;
-            swap(temp_arr[abs(i-j)], temp_arr[i+j-1]);
-            sum += ps[i-j-1]-ps[0];
-            sum += ps[n]-ps[i+j-1];
-            int l = abs(i-j);
-            int r = i+j-1;
-            while (l < r) {
-                sum += temp_arr[l]*arr2[l];
-                sum += temp_arr[r]*arr2[r];
-                l++;
-                r--;
+            if (visited[a.F]) {
+                continue;
             }
-            maxi = max(maxi, sum);
+            point[a.F] = min(point[graph[top].F]+a.S, point[a.F]);
         }
+        visited[top] == true;
+        pq.pop();
     }
-    cout << maxi << endl;
 
     return 0;
 }
+
+void init() {
+    graph[1].push_back(mp(2, 9));
+    graph[1].push_back(mp(3, 4));
+    graph[1].push_back(mp(4, 5));
+
+    graph[2].push_back(mp(5, 5));
+
+    graph[3].push_back(mp(7, 8));
+    graph[3].push_back(mp(8, 10));
+
+    graph[4].push_back(mp(5, 4));
+    graph[4].push_back(mp(3, 2));
+
+    graph[5].push_back(mp(2, 5));
+    graph[5].push_back(mp(4, 9));
+    graph[5].push_back(mp(6, 7));
+    graph[5].push_back(mp(8, 20));
+
+    graph[6].push_back(mp(5, 7));
+    graph[6].push_back(mp(7, 13));
+    graph[6].push_back(mp(9, 14));
+
+    graph[7].push_back(mp(9, 8));
+
+    graph[8].push_back(mp(9, 5));
+}
+
