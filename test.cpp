b3f030ad63ac3cd2aa6e70812babf3360be0f484
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-07-25 22:17:59 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,7 +2,6 @@
 #define int long long
 #define pii pair<int, int>
 #define tp tuple<int, int, int>
-#define db(x) cout << x << " "
 #define mp(x, y) make_pair(x, y)
 #define F first
 #define S second
@@ -10,66 +9,56 @@
 using namespace std;
 __attribute__((optimize("-O3")))
 
-vector<pii> graph[8+1];
-int point[8+1];
-bool visited[8+1];
-int path[8+1][8+1];
+int n, l, w;
+vector<pii> ve;
 
-void init();
+int f(int x, int y) {
+    //cout << endl;
+    auto it = lower_bound(ve.begin(), ve.end(), ( (pii) {x, -1e18} ) );
+    auto it2 = upper_bound(ve.begin(), ve.end(), ( (pii) {x+w+1, -1e18} ) );
+    int result = 0;
+    for (auto a = it; a != it2; a++) {
+        pii b = *a;
+        //cout << b.F << " " << b.S << endl;
+        if (b.S >= y && b.S <= y + l) {
+            result++;
+        }
+    }
+    return result;
+}
+
+void solve() {
+
+    cin >> n >> l >> w;
+    ve.push_back(mp(-1, -1));
+    int maximum_x = -1e18;
+    int maximum_y = -1e18;
+    for (int i = 1; i <= n; i++) {
+        int a, b;
+        cin >> a >> b;
+        maximum_x = max(maximum_x, a);
+        maximum_y = max(maximum_y, b);
+        ve.push_back(mp(a, b));
+    }
+    sort(ve.begin(), ve.end(), less<pii>());
+    int maximum = -1e18;
+    //cout << endl;
+    //cout << (maximum_x) << " " << (maximum_y) << endl;
+    for (int i = 0; i <= maximum_x; i++) {
+        for (int j = 0; j <= maximum_y; j++) {
+            maximum = max(maximum, f(i, j));
+        }
+    }
+    cout << maximum << endl;
+
+}
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    init();
-    memset(&point, (int) 1e18, sizeof(point));
-    priority_queue<int, vector<int>, greater<int> > pq;
-    pq.push(1);
-    point[1] = 0;
-    while (!pq.empty()) {
-        int top = pq.top();
-        for (auto a : graph[top]) {
-            if (top == 1) {
-                point[a.F] = a.S;
-                pq.push(a.F);
-                continue;
-            }
-            if (visited[a.F]) {
-                continue;
-            }
-            point[a.F] = min(point[graph[top].F]+a.S, point[a.F]);
-        }
-        visited[top] == true;
-        pq.pop();
-    }
+    solve();
 
     return 0;
-}
-
-void init() {
-    graph[1].push_back(mp(2, 9));
-    graph[1].push_back(mp(3, 4));
-    graph[1].push_back(mp(4, 5));
-
-    graph[2].push_back(mp(5, 5));
-
-    graph[3].push_back(mp(7, 8));
-    graph[3].push_back(mp(8, 10));
-
-    graph[4].push_back(mp(5, 4));
-    graph[4].push_back(mp(3, 2));
-
-    graph[5].push_back(mp(2, 5));
-    graph[5].push_back(mp(4, 9));
-    graph[5].push_back(mp(6, 7));
-    graph[5].push_back(mp(8, 20));
-
-    graph[6].push_back(mp(5, 7));
-    graph[6].push_back(mp(7, 13));
-    graph[6].push_back(mp(9, 14));
-
-    graph[7].push_back(mp(9, 8));
-
-    graph[8].push_back(mp(9, 5));
 }
 
