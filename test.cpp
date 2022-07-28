06eb485e59bb2a134963f22fb8651513cdc43740
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-07-28 22:34:39 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -12,39 +12,110 @@ __attribute__((optimize("-O3")))
 int n, m;
 vector<int> ve;
 
-bool check(int x) {
-    int k = 1;
-    for (int i = 1; i <= m; i++) {
-        //cout << k << " " << x << " " << (k+x) << "\n";
-        auto it = lower_bound(ve.begin(), ve.end(), k+x);
-        if (it == ve.end()) {
-            return true;
+class seg_tree {
+    public:
+        seg_tree(vector<int> x) {
+            _size = x.size();
+            input.resize(_size);
+            copy(x.begin(), x.end(), input.begin());
+            data.resize(_size*4);
+            data2.resize(_size*4);
+            build(1, _size, 1);
         }
-        k = *it;
-    }
-    return (k >= *ve.rbegin());
-}
+        int query(int l, int r) {
+            return query(l, r, 1, _size, 1);
+        }
+        int query_maxi(int l, int r) {
+            return query_maxi(l, r, 1, _size, 1);
+        }
+
+    private:
+        int _size;
+        vector<int> input;
+        vector<int> data;
+        vector<int> data2;
+        int lc(int x) {
+            return (2*x);
+        }
+        int rc(int x) {
+            return (2*x+1);
+        }
+        void build(int l, int r, int x) {
+            if (l == r) {
+                data[x] = input[l];
+                data2[x] = input[l] % m;
+                //cout << data2[x] << endl;
+                return;
+            }
+            int mid = (l + r) / 2;
+            build(l, mid, lc(x));
+            build(mid+1, r, rc(x));
+            data[x] = data[lc(x)] + data[rc(x)];
+
+            int a = (data2[lc(x)] + data2[rc(x)]) % m;
+            int b = data2[lc(x)] % m;
+            int c = data2[rc(x)] % m;
+
+            data2[x] = max({a, b, c});
+        }
+        int query(int l, int r, int l2, int r2, int x) {
+            //cout << l << " " << r << " " << l2 << " " << r2 << " " << x << endl;
+            if (l == l2 && r == r2) {
+                return data[x];
+            }
+            int mid = (l2 + r2) / 2;
+            int sum = 0;
+            if (r <= mid) {
+                sum += query(l, r, l2, mid, lc(x));
+            }
+            else if (l > mid) {
+                sum += query(l, r, mid+1, r2, rc(x));
+            }
+            else {
+                int a = query(l, mid, l2, mid, lc(x));
+                int b = query(mid+1, r, mid+1, r2, rc(x));
+                sum += a + b;
+            }
+            return sum;
+        }
+        int query_maxi(int l, int r, int l2, int r2, int x) {
+            //cout << l << " " << r << " " << l2 << " " << r2 << " " << x << endl;
+            if (l == l2 && r == r2) {
+                //cout << data2[x] << endl;
+                return data2[x];
+            }
+            int mid = (l2 + r2) / 2;
+            int maxi = -1e18;
+            if (r <= mid) {
+                int a = query_maxi(l, r, l2, mid, lc(x));
+                //cout << a << endl;
+                maxi = max(maxi, a);
+            }
+            else if (l > mid) {
+                int a = query_maxi(l, r, mid+1, r2, rc(x));
+                //cout << a << endl;
+                maxi = max(maxi, a);
+            }
+            else {
+                int a = query_maxi(l, mid, l2, mid, lc(x));
+                int b = query_maxi(mid+1, r, mid+1, r2, rc(x));
+                //cout << a << " " << b << endl;
+                maxi = max({maxi, a, b, (a+b) % m});
+            }
+            return maxi;
+        }
+
+};
 
 void solve() {
 
     cin >> n >> m;
+    ve.resize(n+1);
     for (int i = 1; i <= n; i++) {
-        int a;
-        cin >> a;
-        ve.push_back(a);
+        cin >> ve[i];
     }
-    sort(ve.begin(), ve.end(), less<int>());
-    ve.erase(unique(ve.begin(), ve.end()), ve.end());
-    int l = 0, r = 1e9;
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (check(mid)) {
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
-    }
-    cout << l;
+    seg_tree seg(ve);
+    cout << ( seg.query_maxi(1, n) ) << endl;
 
 }
 
