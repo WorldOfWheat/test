f34539b6c5bb2447188ee4254e3a06a701a05b7f
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-01 02:47:12 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,75 +9,91 @@
 using namespace std;
 __attribute__((optimize("-O3")))
 
-int n, m;
+int n, m, p;
 vector<int> ve;
-vector<int> ve2;
-vector<int> ve_left;
-vector<int> ve_right;
+vector<pii> ve_left;
+vector<pii> ve_right;
+int mid;
+
+vector<int> table(25+1);
+int fact(int x) {
+    if (table[x] != -1) {
+        return table[x];
+    }
+    int a = fact(x-1) * x;
+    table[x] = a;
+    return a;
+}
+void dfs_left(int x, int y, int z) {
+    //cout << x << " " << y << " " << z << endl;
+    if (x == mid) {
+        //cout << y << endl;
+        ve_left.push_back(mp(y, z));
+        return;
+    }
+    dfs_left(x+1, y, z);
+    dfs_left(x+1, y+ve[x], z);
+    if (z < m) dfs_left(x+1, y+fact(ve[x]), z+1);
+}
+
+void dfs_right(int x, int y, int z) {
+    if (x == mid+n%2) {
+        ve_right.push_back(mp(y, z));
+        return;
+    }
+    dfs_right(x+1, y, z);
+    dfs_right(x+1, y+ve[x+mid+1], z);
+    if (z < m) dfs_right(x+1, y+fact(ve[x+mid+1]), z+1);
+}
 
 void solve() {
 
-    cin >> n >> m;
+    fill(table.begin(), table.end(), -1);
+    table[1] = 1;
+
+    cin >> n >> m >> p;
     ve.resize(n+1);
-    ve2.resize(n+1);
-    int mid = n / 2;
-    for (int i = 1; i <= mid; i++) {
+
+    for (int i = 1; i <= n; i++) {
         cin >> ve[i];
     }
-    for (int i = 1; i <= mid+n%2; i++) {
-        cin >> ve2[i];
-    }
-    bitset<40/2+1> bs;
-    int k = 0;
-    while (bs.count() != mid) {
-        bs = k;
-        int sum = 0;
-        for (int i = 1; i <= mid; i++) {
-            //cout << bs[i-1];
-            if (bs[i-1]) {
-                sum += ve[i];
-            }
-        }
-        //cout << endl;
-        ve_left.push_back(sum);
-        k++;
-    }
-    bitset<40/2+1> bs2;
-    k = 0;
-    while (bs2.count() != mid+n%2) {
-        bs2 = k;
-        int sum = 0;
-        for (int i = 1; i <= mid+n%2; i++) {
-            //cout << bs2[i-mid-1];
-            if (bs2[i-1]) {
-                sum += ve2[i];
-            }
-        }
-        //cout << endl;
-        ve_right.push_back(sum);
-        k++;
-    }
-    /*for (auto a : ve_left) {
-        cout << a << " ";
+
+    //sort(ve.begin(), ve.end());
+    //ve.erase( unique(ve.begin(), ve.end()), ve.end());
+
+    mid = n / 2;
+    dfs_left(1, 0, 0);
+    dfs_left(1, ve[mid], 0);
+    if (1 <= m) dfs_left(1, fact(ve[mid]), 1);
+
+    dfs_right(1, 0, 0);
+    dfs_right(1, ve[mid+1], 0);
+    if (1 <= m) dfs_right(1, fact(ve[mid+1]), 1);
+
+    sort(ve_left.begin(), ve_left.end());
+    sort(ve_right.begin(), ve_right.end());
+
+/*
+    for (auto a : ve_left) {
+        cout << a.F << " ";
     }
     cout << endl;
     for (auto a : ve_right) {
-        cout << a << " ";
+        cout << a.F << " ";
     }
-    cout << endl;*/
-    sort(ve_left.begin(), ve_left.end());
-    sort(ve_right.begin(), ve_right.end());
+    cout << endl;
+*/
     int ans = 0;
     for (auto a : ve_left) {
-        int remain = m - a;
-        //cout << a << " " << remain << endl;
-        auto it = upper_bound(ve_right.begin(), ve_right.end(), remain);
-        //cout << distance(ve_right.begin(), it) << endl;
-        if (it == ve_right.begin()) continue;
-        it--;
-        ans = max(ans, *it + a);
+        int remain = p - a.F;
+        //cout <<remain << endl;
+        auto it = lower_bound(ve_right.begin(), ve_right.end(), (pair<int, int>) {remain, -2ll << 31} );
+        auto it2 = upper_bound(ve_right.begin(), ve_right.end(), (pair<int, int>) {remain, 2ll << 31} );
+        cout << (a.S) << " " << ( (*it).S ) << endl;
+        if ( a.S + (*it).S <= m) ans += distance(it, it2);
     }
     cout << ans << endl;
+
 }
 
 signed main() {
