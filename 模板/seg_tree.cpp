386ebb8ca924e3b78dc9ec841c529f89e420831d
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-04 20:11:14 +0800

    Update seg_tree.cpp

--- 模板/seg_tree.cpp
+++ 模板/seg_tree.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 #define int long long
 #define pii pair<int, int>
@@ -10,78 +9,135 @@
 using namespace std;
 __attribute__((optimize("-O3")))
 
+struct node{
+    int left;
+    int right;
+    int lc;
+    int rc;
+    int tag;
+    int sum;
+    int maxi;
+};
+
 class seg_tree {
     public:
-        seg_tree(int x) {
-            data.resize(4*x);
-        }
-        void build (vector<int> x) {
+        seg_tree(vector<int> x) {
             _size = x.size();
-            input.resize(_size);
-            copy(x.begin(), x.end(), input.begin());
+            input.assign(x.begin(), x.end());
+            data.resize(4*_size);
             build(1, _size, 1);
         }
-        int query(int x) {
-            query(x, x);
-        }
-        int query(int l, int r) {
+        node query(int l, int r) {
             return query(l, r, 1, _size, 1);
         }
+        /*void update(int l, int r, int value) {
+            update(l, r, 1, _size, value, 1);
+        }*/
     private:
-        vector<int> input;
-        vector<int> data;
         int _size;
-        int lc(int x) {
-            return (x*2);
+        vector<int> input;
+        vector<node> data;
+        int getLc(int x) {
+            return x << 1;
         }
-        int rc(int x) {
-            return (x*2+1);
+        int getRc(int x) {
+            return (x << 1) + 1;
         }
+        void pull(node &x, node y, node z) {
+            x.left = y.left;
+            x.right = z.right;
+            x.sum = y.sum + z.sum;
+            x.maxi = max(y.maxi, z.maxi);
+        }
+        /*void push(int l, int r, int x) {
+            data[ data[x].lc ].tag = data[x].tag;
+            data[ data[x].rc ].tag = data[x].tag;
+            //cout << data[x].sum << endl;
+            data[x].sum += (r - l + 1) * data[x].tag;
+            //cout << data[x].sum << endl;
+            data[x].tag = 0;
+        }*/
         void build(int l, int r, int x) {
             if (l == r) {
-                data[x] = input[l];
+                data[x].left = data[x].right = data[x].lc = data[x].rc = l;
+                data[x].sum = input[l-1];
+                data[x].maxi = input[l-1];
                 return;
             }
             int mid = (l + r) / 2;
-            build(l, mid, lc(x));
-            build(mid+1, r, rc(x));
-            data[x] = data[lc(x)] + data[rc(x)];
+            build(l, mid, getLc(x));
+            build(mid+1, r, getRc(x));
+            data[x].lc = getLc(x);
+            data[x].rc = getRc(x);
+            pull(data[x], data[ data[x].lc ], data[ data[x].rc ]);
         }
-        int query(int l, int r, int l2, int r2, int x) {
+        node query(int l, int r, int l2, int r2, int x) {
             if (l == l2 && r == r2) {
                 return data[x];
             }
-            int sum = 0;
-            int mid = (l2 + r2) / 2;
+            //push(l, r, x);
+            int mid = (l2 + r2) >> 1;
             if (r <= mid) {
-                sum += query(l, r, l2, mid, lc(x));
+                return query(l, r, l2, mid, getLc(x));
             }
             else if (l > mid) {
-                sum += query(l, r, mid+1, r2, rc(x));
+                return query(l, r, mid+1, r2, getRc(x));
             }
             else {
-                sum += query(l, mid, l2, mid, lc(x)) + query(mid+1, r, mid+1, r2, rc(x));
+                node a = query(l, mid, l2, mid, getLc(x));
+                node b = query(mid+1, r, mid+1, r2, getRc(x));
+                node result;
+                pull(result, a, b);
+                return result;
             }
-            return sum;
         }
+        /*void update(int l, int r, int l2, int r2, int value, int x) {
+            if (l == l2 && r == r2) {
+                //cout << l << " " <<r <<endl;
+                data[x].tag += value;
+                return;
+            }
+            int mid = (l2 + r2) >> 1;
+            if (r <= mid) {
+                update(l, r, l2, mid, value, getLc(x));
+            }
+            else if (l > mid) {
+                update(l, r, mid+1, r2, value, getRc(x));
+            }
+            else {
+                update(l, mid, l2, mid, value, getLc(x));
+                update(mid+1, r, mid+1, r2, value, getRc(x));
+            }
+            pull(data[x], data[ data[x].lc ], data[ data[x].rc ]);
+            return;
+        }*/
 };
 
-vector<int> ve = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int n, m;
+vector<int> ve;
 
 void solve() {
 
-    seg_tree seg(ve.size());
-    seg.build(ve);
-    cout << (seg.query(1, 9)) << endl;
+    ve.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> ve[i];
+    }
+    seg_tree seg(ve);
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << (seg.query( min(a, b), max(a, b) ).maxi) << "\n";
+    }
 
+    return;
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    solve();
+    while (cin >> n) solve();
 
     return 0;
 }
-
