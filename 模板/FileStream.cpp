83e6986d4f6830ea89c9f03d4014d30d609457c1
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-13 02:20:28 +0800

    Create FileStream.cpp

---
+++ 模板/FileStream.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int, int>
+#define tp tuple<int, int, int>
+#define F first
+#define S second
+
+using namespace std;
+
+void solve() {
+
+    ifstream in;
+    ofstream out;
+    in.open("test_input.txt");
+    if (in.fail()) {
+        cout << "ERROR - 1" << "\n";
+        return;
+    }
+    out.open("test_output.txt");
+    if (out.fail()) {
+        cout << "ERROR - 2" << "\n";
+        return;
+    }
+  
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
