1009f6c44e290c6da73a0af407004c9349e64b44
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-15 09:11:53 +0800

    Update 預設.cpp

--- 模板/預設.cpp
+++ 模板/預設.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 #define int long long
 #define pii pair<int, int>
-#define tp tuple<int, int, int>
-#define mp(x, y) make_pair(x, y)
 #define F first
 #define S second
+#define ln '\n'
 
 using namespace std;
 
