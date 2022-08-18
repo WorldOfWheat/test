f33156ce1d375c40800644e239537543e6d2cc0a
WorldOfWheat <86335529+WorldOfWheat@users.noreply.github.com>
2022-08-19 02:18:42 +0800

    Update bigNumberCal.cpp

--- 模板/bigNumberCal.cpp
+++ 模板/bigNumberCal.cpp
@@ -1,232 +1,147 @@
-#include <bits/stdc++.h>
-#define int long long
-#define pii pair<int, int>
-#define tp tuple<int, int, int>
-#define mp(x, y) make_pair(x, y)
-#define F first
-#define S second
-
-using namespace std;
-using cd = complex<double>;
-__attribute__((optimize("-O3")))
-
-class bigIntegerCal{
+class bigIntegerCal {
     public:
-        stack<int> add(vector<int> x, vector<int> y) {
-            const int len_x = x.size();
-            const int len_y = y.size();
-            int ptr_x = len_x - 1;
-            int ptr_y = len_y - 1;
-            stack<int> st_result;
+        vector<int> add(vector<int> x, vector<int> y) {
+            int l = x.size() - 1;
+            int r = y.size() - 1;
+            vector<int> res;
             int carry = 0;
-            while (ptr_x >= 0 && ptr_y >= 0) {
-                int a = x[ptr_x] + y[ptr_y] + carry;
-                st_result.push(a % 10);
+            while (l >= 0 && r >= 0) {
+                int a = x[l] + y[r] + carry;
+                res.push_back(a % 10);
                 carry = a / 10;
-                ptr_x--;
-                ptr_y--;
+                l--;
+                r--;
             }
-            while (ptr_x >= 0) {
-                int a = x[ptr_x] + carry;
-                st_result.push(a % 10);
+            while (l >= 0) {
+                int a = x[l] + carry;
+                res.push_back(a % 10);
                 carry = a / 10;
-                ptr_x--;
+                l--;
             }
-            while (ptr_y >= 0) {
-                int a = y[ptr_y] + carry;
-                st_result.push(a % 10);
+            while (r >= 0) {
+                int a = y[r] + carry;
+                res.push_back(a % 10);
                 carry = a / 10;
-                ptr_y--;
+                r--;
             }
-            return st_result;
+            if (carry != 0) {
+                res.push_back(carry);
+            }
+            return res;
         }
-        stack<int> sub(vector<int> x, vector<int> y) {
-            const int len_x = x.size();
-            const int len_y = y.size();
-            int ptr_x = len_x - 1;
-            int ptr_y = len_y - 1;
-            stack<int> st_result;
-            while (ptr_x >= 0 && ptr_y >= 0) {
-                int a = x[ptr_x] - y[ptr_y];
-                if (a >= 0) {
-                    st_result.push(a);
+        vector<int> sub(vector<int> x, vector<int> y) {
+            int l = x.size() - 1;
+            int r = y.size() - 1;
+            vector<int> res;
+            int carry = 0;
+            while (l >= 0 && r >= 0) {
+                if (x[l] - y[r] < 0) {
+                    int a = 10 + x[l] - y[r];
+                    res.push_back(a);
+                    x[l-1]--;
                 }
                 else {
-                    st_result.push(a + 10);
-                    x[ptr_x - 1]--;
+                    res.push_back(x[l] - y[r]);
                 }
-                ptr_x--;
-                ptr_y--;
+                l--;
+                r--;
             }
-            while (ptr_x >= 0) {
-                if (x[ptr_x] < 0) {
-                    x[ptr_x-1]--;
-                    st_result.push(x[ptr_x] + 10);
+            while (l >= 0) {
+                if (x[l] < 0) {
+                    x[l-1]--;
+                    res.push_back(10 + x[l]);
                 }
                 else {
-                    if (ptr_x == 0 && x[ptr_x] == 0) {
+                    if (l == 0 && x[l] == 0) {
                         break;
                     }
-                    st_result.push(x[ptr_x]);
+                    res.push_back(x[l]);
                 }
-                ptr_x--;
+                l--;
             }
-            return st_result;
+            return res;
         }
-        stack<int> mul(vector<int> x, vector<int> y) {
-            vector<cd> ve_cd(x.begin(), x.end());
-            vector<cd> ve_cd2(y.begin(), y.end());
-
+        vector<int> mul(vector<int> x, vector<int> y) {
             int _size = 1;
-            while (_size < x.size() + y.size()) {
+            int a = x.size();
+            int b = y.size();
+            while (_size < (a + b - 1)) {
                 _size <<= 1;
             }
 
-            ve_cd.resize(_size);
-            ve_cd2.resize(_size);
+            x.resize(_size);
+            y.resize(_size);
 
-            fft(ve_cd);
-            fft(ve_cd2);
+            ntt(x, _size, false);
+            ntt(y, _size, false);
 
-            for (int i = 0; i < ve_cd.size(); i++) {
-                ve_cd[i] *= ve_cd2[i];
+            for (int i = 0; i < _size; i++) {
+                x[i] = 1ll * x[i] * y[i] % P;
             }
 
-            reverse_fft(ve_cd);
+            ntt(x, _size, true);
 
-            int remain = ( _size - (x.size() + y.size()) ) + 1;
-            stack<int> st_result;
+            int remain = (_size - (a + b)) + 1;
+            vector<int> res;
             int carry = 0;
-            for (int i = _size - remain - 1; i >= 0 ; i--) {
-                int a = round(ve_cd[i].real());
-                int b = carry + a;
-                //cout << a << " " << b << endl;
-                st_result.push(b % 10);
-                carry = b / 10;
-                //ve_result[i] = round(ve_cd[i].real());
+            for (int i = _size - remain - 1; i >= 0; i--) {
+                int a = carry + x[i];
+                res.push_back(a % 10);
+                carry = a / 10;
             }
             if (carry != 0) {
-                st_result.push(carry);
+                res.push_back(carry);
             }
-            return st_result;
+            return res;
+        }
+
+        void ntt(vector<int> &x, int _size, bool oper) {
+            for (int i = 1, o = 0; i < _size; i++) {
+                int p = _size >> 1;
+                o ^= p;
+                while (!(o & p)) {
+                    p >>= 1;
+                    o ^= p;
+                }
+                if (i < o) {
+                    swap(x[i], x[o]);
+                }
+            }
+            for (int i = 2; i <= _size; i <<= 1) {
+                int p = i >> 1;
+                int gn = fastpow(G, (P - 1) / i);
+                for (int j = 0; j < _size; j+=i) {
+                    int g = 1;
+                    for (int k = 0; k < p; k++) {
+                        int temp = 1ll * x[j+k+p] * g % P;
+                        x[j+k+p] = (x[j+k] - temp + P) % P;
+                        x[j+k] = (x[j+k] + temp) % P;
+                        g = 1ll * g * gn % P;
+                    }
+                }
+            }
+            if (oper) {
+                reverse(x.begin()+1, x.begin() + _size);
+                int inv = fastpow(_size, P-2);
+                for (int i = 0; i < _size; i++) {
+                    x[i] = 1ll * x[i] * inv % P;
+                }
+            }
         }
 
     private:
-        const double PI = acos(-1);
+        const int G = 3, P = 998244353;
 
-        void fft(vector<cd> &x) {
-            const int _size = x.size();
-            if (_size == 1) {
-                return;
+        int fastpow(int x, int y) {
+            int sum = 1;
+            while (y > 0) {
+                if (y & 1) {
+                    sum = 1ll * sum * x % P;
+                }
+                y >>= 1;
+                x = 1ll * x * x % P;
             }
-            int mid = _size >> 1;
-            vector<cd> left(mid);
-            vector<cd> right(mid);
-            for (int i = 0; i < mid; i++) {
-                left[i] = x[i << 1];
-                right[i] = x[(i << 1) + 1];
-            }
-            fft(left);
-            fft(right);
-            const double angle = PI / _size * 2;
-            cd w(1), wn(cos(angle), sin(angle));
-            for (int i = 0; i < mid; i++) {
-                x[i] = left[i] + w * right[i];
-                x[i + mid] = left[i] - w * right[i];
-                w *= wn;
-            }
+            return sum;
         }
 
-        void reverse_fft(vector<cd> &x) {
-            int _size = x.size();
-            if (_size == 1) {
-                return;
-            }
-            int mid = _size >> 1;
-            vector<cd> left(mid);
-            vector<cd> right(mid);
-            for (int i = 0; i < mid; i++) {
-                left[i] = x[i << 1];
-                right[i] = x[(i << 1) + 1];
-            }
-            reverse_fft(left);
-            reverse_fft(right);
-            const double angle = PI / _size * -1 * 2;
-            cd w(1), wn(cos(angle), sin(angle));
-            for (int i = 0; i < mid; i++) {
-                x[i] = left[i] + w * right[i];
-                x[i + mid] = left[i] - w * right[i];
-                x[i] /= 2;
-                x[i + mid] /= 2;
-                w *= wn;
-            }
-        }
 };
-
-char input;
-vector<int> ve;
-vector<int> ve2;
-char symbol;
-
-void solve() {
-
-    bool flag = false;
-    while (input != EOF) {
-        input = getchar();
-        if (input != ' ') {
-            if ( isdigit(input) ) {
-                if (!flag) {
-                    ve.push_back(input - 48);
-                }
-                else {
-                    ve2.push_back(input - 48);
-                }
-            }
-            if ( ispunct(input) ) {
-                symbol = input;
-                flag = true;
-            }
-        }
-    }
-    bigIntegerCal big;
-    switch (symbol) {
-        case '+': {
-            stack<int> st = big.add(ve, ve2);
-            while (!st.empty()) {
-                putchar(st.top() + 48);
-                //cout << (st.top());
-                st.pop();
-            }
-            break;
-        }
-        case '-': {
-            stack<int> st = big.sub(ve, ve2);
-            while (!st.empty()) {
-                putchar(st.top() + 48);
-                //cout << (st.top());
-                st.pop();
-            }
-            break;
-        }
-        case '*': {
-            stack<int> st = big.mul(ve, ve2);
-            while (!st.empty()) {
-                putchar(st.top() + 48);
-                //cout << (st.top());
-                st.pop();
-            }
-            break;
-        }
-    }
-    cout << "\n";
-
-}
-
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    solve();
-
-    return 0;
-}
