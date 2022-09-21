cc17f59b5e6d83ce0ea3f601f09f787c92338b27
WorldOfWheat <a302854888@gmail.com>
2022-09-21 22:11:24 +0800

    modified:   test.cpp 	modified:   test.exe

--- test.cpp
+++ test.cpp
@@ -12,73 +12,18 @@
 #define ln "\n"
 #define sp " "
 #define INF (int) 1e18
+#define MOD (int) 1e9 + 9
 
 using namespace std;
 
-string str, str2;
-VV dp;
-VV status;
+
 
 void solve() {
 
-	dp.clear();
-	status.clear();
+	int n;
+	cin >> n;
 
-	cout << str << sp << str2 << " = ";
-
-	int len = str.length();
-	int len2 = str2.length();
 	
-	dp.resize(len+1, V(len2+1));
-	status.resize(len+1, V(len2+1));
-
-	rep2 (i, 1, len) {
-		rep2 (j, 1, len2) {
-			char top = str[i-1];
-			char top2 = str2[j-1];
-
-			if (top == top2) {
-				dp[i][j] = dp[i-1][j-1] + 1;
-				status[i][j] = 1;
-				continue;
-			}
-
-			if (dp[i-1][j] > dp[i][j-1]) {
-				dp[i][j] = dp[i-1][j];
-				status[i][j] = 2;
-			}
-			else {
-				dp[i][j] = dp[i][j-1];
-				status[i][j] = 3;
-			}
-		}
-	}
-
-	stringstream ss;
-
-	int nowX = len;
-	int nowY = len2;
-	
-	while (nowX != 0 && nowY != 0) {
-		switch (status[nowX][nowY]) {
-		case 1:			
-			nowX--;
-			nowY--;
-			ss << str[nowX-1];
-			break;
-		case 2:
-			nowX--;
-		case 3:
-			nowY--;
-		}
-	}
-
-	str = ss.str();
-
-	for (auto i = str.rbegin(); i != str.rend(); i++) {
-		cout << (*i);
-	}
-	cout << ln;
 
 }
 
@@ -87,7 +32,7 @@ signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	while (cin >> str >> str2) solve();
+	solve();
 
 	return 0;
 
