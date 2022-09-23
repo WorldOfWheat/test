165dd8302742ef4757c0d88b05a32578909cab1c
WorldOfWheat <a302854888@gmail.com>
2022-09-23 20:30:24 +0800

    modified:   test.cpp 	modified:   test.exe

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 #define int long long
 #define V vector<int>
 #define VP vector<pii>
-#define VV vector<vector<int>>
+#define vv vector<vector<int>>
 #define pii pair<int, int>
 #define F first
 #define S second
@@ -12,15 +12,42 @@
 #define ln "\n"
 #define sp " "
 #define INF (int) 1e18
-#define MOD (int) 1e9 + 9
 
 using namespace std;
 
-
+V ve;
+V dp;
+deque<int> de;
 
 void solve() {
+	
+	int n, m;
+	cin >> n >> m;
+
+	m++;
+
+	ve.resize(n);
+	dp.resize(n+1);
+
+	rep (i, 0, n) {
+		cin >> ve[i];
+	}
 
 	
+
+	rep (i, 0, n) {						
+		while (de.size() && de.front() < i - 2*m) {
+			de.pop_front();
+		}		
+
+		while (de.size() && ve[de.back()] >= ve[i]) {
+			de.pop_back();
+		}
+
+		de.push_back(i);
+	
+
+	}
 
 }
 
