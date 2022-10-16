831c14be84bf9f436b9cf2384cbc9b90b372ba1d
小麥 <a302854888@gmail.com>
2022-10-16 22:58:49 +0800

    modified:   tutorial.cpp

--- tutorial.cpp
+++ tutorial.cpp
@@ -6,22 +6,7 @@ int arr[26+1];
 
 int main() {
 
-    string str, str2;
-    cin >> str >> str2;
-
-    string res = "";
-    string temp = "";
-
-    for (int i = 0; i < str2.length(); i++) {
-        temp += str[i] - (str2[i] - 'A');
-    }
-
-    res += temp;
-
-    for (int i = str2.length(); i < str.length();) {
-            
-    }
-    cout << res << "\n";
+    
 
     return 0;
 
