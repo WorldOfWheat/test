0ca081e057f1950865331cc22027db3182e7503c
WorldOfWheat <a302854888@gmail.com>
2023-05-31 23:34:47 +0800

    test commit

--- Python/test.py
+++ Python/test.py
@@ -1,26 +1,26 @@
 import os
 
 def main():
-    print("-------------------------------------------------")
+    print("-----------------------------------")
     print("Please input the commit message:")
-    print("-------------------------------------------------")
+    print("-----------------------------------")
     commit_message = ""
     commit_message = input();
 
     os.system("git add .")
     os.system("git commit -S -a -m " + "\"" + commit_message + "\"")
-    # os.system("git push origin main")
+    os.system("git push origin main")
 
-    print("-------------------------------------------------")
+    print("----------------------------------------------")
     print("Should run again? Input '1' or '0' to select:")
-        print("----------------------------------------------------------------")
+    print("----------------------------------------------")
     while (True):
         select_input = input()
         if select_input == "1" or select_input == "0":
             break
-        print("-----------------------------------------------------")
+        print("-----------------------------------------")
         print("Wrong Input! Input Should be '1' or '0'!")
-        print("-----------------------------------------------------")
+        print("-----------------------------------------")
     again = bool(int(select_input));
     if again:
         main()
