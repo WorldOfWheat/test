2af0cb998342305d22ca0dc4281c4a57c1a0d202
WorldOfWheat <a302854888@gmail.com>
2023-06-01 00:09:57 +0800

    c# oop test

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -1,59 +1,77 @@
 ﻿using System.Runtime.CompilerServices;
 
-class Fruit
+
+namespace OOP_Test2
 {
-    protected string Name;
-    protected int Count;
-}
-
-class Apple : Fruit
-{
-    public Apple(string NewName, int Increasement)
+    internal class Fruit
     {
-        this.Name = NewName;
-        this.Count += Increasement;
-    }
-    public void GetApple()
-    {
-        Console.WriteLine($"{this.Name} x {Count}");
-    }
-    public void SetApple(int Increasement)
-    {
-        this.Count += Increasement;
-    }
-}
-
-class Program
-{
-    static int a = 0;
-    static SemaphoreSlim semaphore = new SemaphoreSlim(1);
-
-    public static async Task Main(string[] args)
-    {
-        Apple a = new Apple("test", 5);
-        a.GetApple();
-        a.SetApple(500);
-        a.Name = "test";
+        protected string Name;
+        protected int Count;
+        internal int test;
     }
 
-    public static async Task Run1()
+
+    class Apple : Fruit
     {
-        Run2();
-        for (int i = 0; i < 10; i++)
+        public Apple(string NewName, int Increasement)
         {
-            await Task.Delay(2);
-            Console.Write($"{i} ");
+            this.Name = NewName;
+            this.Count += Increasement;
+            test = Increasement;
         }
-        Console.WriteLine("");
-    }
-    public static async Task Run2()
-    {
-        for (int i = 10; i < 20; i++)
+        public void GetApple()
         {
-            await Task.Delay(1);
-            Console.Write($"{i} ");
+            Console.WriteLine($"{this.Name} x {Count}");
         }
-        Console.WriteLine("");
+        public void SetApple(int Increasement)
+        {
+            this.Count += Increasement;
+        }
+    }
+
+    class Program
+    {
+        static int a = 0;
+        static SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+        public static async Task Main(string[] args)
+        {
+            Apple a = new Apple("test", 5);
+            a.GetApple();
+            a.SetApple(500);
+        }
+
+        public static async Task Run1()
+        {
+            Run2();
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(2);
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("");
+        }
+        public static async Task Run2()
+        {
+            for (int i = 10; i < 20; i++)
+            {
+                await Task.Delay(1);
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("");
+        }
     }
 }
 
+namespace OOP_Test
+{
+    class Car
+    {
+        OOP_Test2.Fruit fruit = new OOP_Test2.Fruit();
+        public void drive()
+        {
+            fruit.test = 5;
+        }
+    }
+
+}
