6a94506694d94bb96c263e2d47f9d17ca7ce6e7e
WorldOfWheat <a302854888@gmail.com>
2023-06-05 03:41:36 +0800

    cs cyphersecurity test, xor swap

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -3,13 +3,43 @@ using System.Text;
 
 class Program
 {
+    static RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+
+	static void GenerateCipherText(ref byte[] CipherText, ref byte[] Key, ref byte[] Nonce, ref byte[] Tag)
+	{
+		Key = new byte[32];
+		randomNumberGenerator.GetBytes(Key);
+		Nonce = new byte[12];	 
+		randomNumberGenerator.GetBytes(Nonce);
+		var ChaCha20 = new ChaCha20Poly1305(Key);
+		var PlainText = new byte[128];
+		randomNumberGenerator.GetBytes(PlainText);
+
+		Console.WriteLine($"PlainText:\n{BitConverter.ToString(PlainText).Replace("-", "")}"); 
+
+		CipherText = new byte[PlainText.Length];
+		Tag = new byte[16];
+
+		ChaCha20.Encrypt(Nonce, PlainText, CipherText, Tag);
+	}
+	
 	public static void Main(string[] args)
 	{
-		HMACSHA256 hMACSHA256 = new HMACSHA256();
-		hMACSHA256.Initialize();
-		var key = Encoding.UTF8.GetBytes("tesat");
-		hMACSHA256.Key = key;
-		var result = hMACSHA256.ComputeHash(Encoding.UTF8.GetBytes("message"));
-		Console.WriteLine(BitConverter.ToString(result));
+		byte[] Key = new byte[32];
+		byte[] Nonce = new byte[12];
+		byte[] CipherText = new byte[128];
+		byte[] Tag = new byte[16];
+
+		GenerateCipherText(ref CipherText, ref Key, ref Nonce, ref Tag);
+		
+		Console.WriteLine($"CipherText:\n{BitConverter.ToString(CipherText).Replace("-", "")}"); 
+		Console.WriteLine($"Tag:\n{BitConverter.ToString(Tag).Replace("-", "")}"); 
+
+		var ChaCha20 = new ChaCha20Poly1305(Key);
+		var PlainText = new byte[128];
+		// Console.WriteLine(PlainText.Length + " " + CipherText.Length);
+		ChaCha20.Decrypt(Nonce, CipherText, Tag, PlainText);
+
+		Console.WriteLine($"PlainText:\n{BitConverter.ToString(PlainText).Replace("-", "")}"); 
 	}
 }
---
+++ C#/test/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Sum(ref int Sum, int[] Array)
+    {
+        Sum = 0;
+        foreach (var i in Array)
+        {
+            Sum += i;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        var array = new int[32];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i;
+        }
+        Random rand = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            int randNumber = rand.Next(0, array.Length);
+            if (i == randNumber)
+            {
+                continue;
+            }
+            array[i] ^= array[randNumber];
+            array[randNumber] ^= array[i];
+            array[i] ^= array[randNumber];
+        }
+
+        foreach (var i in array)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine("");
+        
+        int Result = 0;
+        Sum(ref Result, array);
+        Console.WriteLine(Result);
+    }
+}
