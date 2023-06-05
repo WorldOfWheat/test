373accef6c1a30940430ecc458459f7d394f7d5d
WorldOfWheat <a302854888@gmail.com>
2023-06-06 03:07:00 +0800

    cs rsa test

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -14,10 +14,14 @@ class Program
 
 	public static void Main(string[] args)
 	{
-
-		// RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(4096);
-		// string PublicKey = BitConverter.ToString(rsa.ExportRSAPublicKey()).Replace("-", "");
-		// string PrivateKey = BitConverter.ToString(rsa.ExportRSAPrivateKey()).Replace("-", "");
-		// Console.WriteLine($"PublicKey: {PublicKey}\nPrivateKey: {PrivateKey}");
+		RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(1024);
+		string PublicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey()).Replace("-", "");
+		string PrivateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey()).Replace("-", "");
+		Console.WriteLine($"PublicKey: {PublicKey}\nPrivateKey: {PrivateKey}");
+		byte[] PlainText = Encoding.UTF8.GetBytes("Secure message");
+		byte[] CipherText = rsa.Encrypt(PlainText, true);
+		Console.WriteLine($"CipherText: {Convert.ToBase64String(CipherText).Replace("-", "")}");
+		byte[] PlainText2 = rsa.Decrypt(CipherText, true);
+		Console.WriteLine($"PlainText: {Encoding.UTF8.GetString(PlainText2)}");
 	}
 }
