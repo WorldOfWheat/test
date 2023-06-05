46402efb9d1a423ecdd0b41e490e3d29630a0a70
WorldOfWheat <a302854888@gmail.com>
2023-06-05 14:14:36 +0800

    python push tool update, c# cyphersecurity test

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -3,43 +3,7 @@ using System.Text;
 
 class Program
 {
-    static RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
-
-	static void GenerateCipherText(ref byte[] CipherText, ref byte[] Key, ref byte[] Nonce, ref byte[] Tag)
-	{
-		Key = new byte[32];
-		randomNumberGenerator.GetBytes(Key);
-		Nonce = new byte[12];	 
-		randomNumberGenerator.GetBytes(Nonce);
-		var ChaCha20 = new ChaCha20Poly1305(Key);
-		var PlainText = new byte[128];
-		randomNumberGenerator.GetBytes(PlainText);
-
-		Console.WriteLine($"PlainText:\n{BitConverter.ToString(PlainText).Replace("-", "")}"); 
-
-		CipherText = new byte[PlainText.Length];
-		Tag = new byte[16];
-
-		ChaCha20.Encrypt(Nonce, PlainText, CipherText, Tag);
-	}
-	
 	public static void Main(string[] args)
 	{
-		byte[] Key = new byte[32];
-		byte[] Nonce = new byte[12];
-		byte[] CipherText = new byte[128];
-		byte[] Tag = new byte[16];
-
-		GenerateCipherText(ref CipherText, ref Key, ref Nonce, ref Tag);
-		
-		Console.WriteLine($"CipherText:\n{BitConverter.ToString(CipherText).Replace("-", "")}"); 
-		Console.WriteLine($"Tag:\n{BitConverter.ToString(Tag).Replace("-", "")}"); 
-
-		var ChaCha20 = new ChaCha20Poly1305(Key);
-		var PlainText = new byte[128];
-		// Console.WriteLine(PlainText.Length + " " + CipherText.Length);
-		ChaCha20.Decrypt(Nonce, CipherText, Tag, PlainText);
-
-		Console.WriteLine($"PlainText:\n{BitConverter.ToString(PlainText).Replace("-", "")}"); 
 	}
 }
--- Python/PushTool/main.py
+++ Python/PushTool/main.py
@@ -1,14 +1,20 @@
 import os
 
 def main():
+    os.system("git status") # 顯示所有變改
+
     print("-----------------------------------")
     print("Please input the commit message:")
     print("-----------------------------------")
+    
+    # 輸入 commit message
     commit_message = ""
     commit_message = input();
 
+    # 重新push用
     again = True
     while (again):
+        # 上傳指令
         os.system("git add .")
         os.system("git commit -S -a -m " + "\"" + commit_message + "\"")
         os.system("git push origin main")
@@ -17,6 +23,7 @@ def main():
         print("Should run again? Input '1' or '0' to select:")
         print("----------------------------------------------")
 
+        # 待輸入是否重新執行
         while (True):
             select_input = input()
             if select_input == "1" or select_input == "0":
