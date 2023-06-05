1e569d27ba41e21650e8f9496c0e2d5e5eb98494
WorldOfWheat <a302854888@gmail.com>
2023-06-05 23:58:08 +0800

    cs cryptography test

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -3,7 +3,21 @@ using System.Text;
 
 class Program
 {
+	static void PrintBytesArray(byte[] Input)
+	{
+		foreach (byte i in Input)
+		{
+			Console.Write(i + " ");
+		}
+		Console.WriteLine();
+	}
+
 	public static void Main(string[] args)
 	{
+
+		// RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(4096);
+		// string PublicKey = BitConverter.ToString(rsa.ExportRSAPublicKey()).Replace("-", "");
+		// string PrivateKey = BitConverter.ToString(rsa.ExportRSAPrivateKey()).Replace("-", "");
+		// Console.WriteLine($"PublicKey: {PublicKey}\nPrivateKey: {PrivateKey}");
 	}
 }
