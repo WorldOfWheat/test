aa7d26bcf6f7dc925597ccfa95f5edd151926fb7
WorldOfWheat <a302854888@gmail.com>
2023-06-07 02:13:55 +0800

    cs stream test

---
+++ C#/test2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text;
+using System.Net.Sockets;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		string Path = @"C:\temp\";
+		if (!Directory.Exists(Path))
+		{
+			Directory.CreateDirectory(Path);
+		}
+		if (!File.Exists(Path + "test.txt"))
+		{
+			File.Create(Path + "test.txt").Close();
+		}
+		Random random = new Random(6);
+		byte[] Data = Encoding.UTF8.GetBytes("abcdefghijklmnopqrstuvwxyz");
+		byte[] Mask = new byte[Data.Length];
+		random.NextBytes(Mask);
+		for (int i = 0; i < Mask.Length; i++)
+		{
+			Data[i] = (byte)(Data[i] ^ Mask[i]);
+		}
+		using (FileStream fs = new FileStream(Path + "test.txt", FileMode.Create, FileAccess.Write))
+		using (BinaryWriter bw = new BinaryWriter(fs))
+		{
+			bw.Write(Data);
+		}
+	}
+}
