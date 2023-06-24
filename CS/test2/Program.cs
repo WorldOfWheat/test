fc885bae9b5e8123b2cf7e9208cf5e06fbc2c903
WorldOfWheat <a302854888@gmail.com>
2023-06-25 03:56:44 +0800

    C# file comprasion test

--- CS/test2/Program.cs
+++ CS/test2/Program.cs
@@ -1,12 +1,47 @@
-﻿using System.IO;
-using System.Text;
-using System.Security.Cryptography;
-
-class Program
+﻿class Program
 {
+	static SortedSet<string> allPaths = new SortedSet<string>(); 
+
+	static void getAllPaths(string rootPath)
+	{
+		if (Directory.Exists(rootPath)) 
+		{
+			foreach (var i in Directory.GetDirectories(rootPath))
+			{
+				getAllPaths(i);
+			}
+		}
+
+		foreach (var i in Directory.GetFiles(rootPath))
+		{
+			allPaths.Add(i);
+		}
+	}
+	
 	public static void Main(string[] args)
 	{
-        int processorCount = Environment.ProcessorCount;
-        Console.WriteLine("可用核心數量：" + processorCount);
+		string path = @"d:\軟體\auto click.exe";
+		allPaths.Add(@"D:\軟體\auto click.exe");
+		allPaths.Add(@"D:\軟體\Sandboxie-Plus\Qt5Core.dll");
+		allPaths.Add(@"D:\軟體\Sandboxie-Plus\Qt5Gui.dll");
+		allPaths.Add(@"D:\軟體\Sandboxie-Plus\SbieIni.exe");
+		allPaths.Add(@"D:\軟體\LocalSend\flutter_windows.dll");
+		// SortedSet<string> set = new SortedSet<string>();
+		// getAllPaths(path); 
+		foreach (var i in allPaths)
+		{
+			using (FileStream fileStream = File.OpenRead(i))
+			using (BinaryReader reader = new BinaryReader(fileStream))
+			{
+
+				List<byte> comparisonBytes = new List<byte>();
+				for (int j = 1; j < fileStream.Length; j *= 2)
+				{
+					fileStream.Seek(j, SeekOrigin.Begin);
+					comparisonBytes.Add(reader.ReadByte());
+				}
+				Console.WriteLine(Convert.ToBase64String(comparisonBytes.ToArray()));
+			}
+		}
 	}
 }
