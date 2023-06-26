6162e01455524f286bcc7d5c822bc7a8285484fb
WorldOfWheat <a302854888@gmail.com>
2023-06-27 02:58:44 +0800

    c++ test

--- C_C++/test3.cpp
+++ C_C++/test3.cpp
@@ -1,22 +1,99 @@
-#include <bits/stdc++.h>
+// 實作B
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    // 1.
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << i << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
 
-    // Open file
-    fstream myFile;
-    myFile.open("test.txt", ios::app);
+    // 2
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << j << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
 
-    // Write to the file
-    myFile << "\nI want to go to play.";
+    // 3
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    cout << endl;
 
-    // Close file
-    myFile.close();
+    // 4
+    int counter = 1;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << counter << '\t';
+            counter++;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // 5
+    for (int i = 1; i <= 5; i++) 
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // 6
+    for (int i = 1; i <= 5; i++) 
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // 7
+    for (int i = 1; i <= 5; i++) 
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // 8
+    for (int i = 5; i >= 1; i--) 
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    cout << endl;
 
     return 0;
 }
