cd8626365f102c5ffdbf9b518c249910c64b8769
WorldOfWheat <a302854888@gmail.com>
2023-06-30 00:18:05 +0800

    clean

--- CS/ConsoleApp2/Program.cs
+++ CS/ConsoleApp2/Program.cs
@@ -1,30 +1,7 @@
-﻿using System.Diagnostics;
-using Org.BouncyCastle.Crypto.Digests;
-
-class Program
+﻿class Program
 {
-    private static FileStream? fileStream;
-    private static BinaryReader? binaryReader;
-    // private static Semaphore semaphore = new Semaphore(20, 20);
-
-    
-
     public static void Main(string[] args)
     {
-        Stopwatch stopWatch = new Stopwatch();
-        stopWatch.Start();        
-        
-        string path = @"C:\Users\USER\Downloads\Array30IME.zip";
-        fileStream = File.OpenRead(path);
-        binaryReader = new BinaryReader(fileStream);
-        long plainTextLength = fileStream.Length;
 
-        Console.WriteLine($"PlainText length: {plainTextLength}");
-        byte[] hash = CalculateSha256(0, plainTextLength);
-        Console.WriteLine(Convert.ToHexString(hash).Replace("-", " "));
-
-        stopWatch.Stop();
-
-        Console.WriteLine("Time (ms): " + stopWatch.ElapsedMilliseconds);
     }
 }
