6a36059cceb70c645dedc6f60ad81b3d14ae4a9f
WorldOfWheat <a302854888@gmail.com>
2023-06-29 12:40:06 +0800

    algorithm test, segment tree test

--- C_C++/main.cpp
+++ C_C++/main.cpp
@@ -11,22 +11,35 @@
 #define rep(x, y, z) for(int x = y; x < z; x++)
 #define rep2(x, y, z) for(int x = y; x <= z; x++)
 #define rrep(x, y, z) for(int x = y; x >= z; x--)
+#define nl '\n';
 #define INF (int) 1e18
+#define MOD (int) 1e9 + 7
 
 using namespace std;
+
+class NodeData
+{
+public:
+    static NodeData combine(NodeData a, NodeData b)
+    {
+        NodeData combined;
+        combined.sum = a.sum + b.sum;
+    }
+    int sum;
+};
 
 class TreeNode
 {
 public:
     TreeNode()
     {
-        Left = NULL;
-        Right = NULL;
-        value = 0;
+        left = NULL;
+        right = NULL;
     }
-    TreeNode* Left;
-    TreeNode* Right;
-    int value;
+
+    TreeNode* left;
+    TreeNode* right;
+    NodeData data;
 };
 
 class SegmentTree
@@ -34,17 +47,58 @@ class SegmentTree
 public:
     void build(vector<int> &arr)
     {
-        build(arr, 0, arr.size());
+        size = arr.size();
+        build(arr, root, 0, size);
     }
-private:
-    TreeNode* root;
-    void add(vector<int> &arr, int left, int right, int index)
+    NodeData query(int index)
     {
-        int mid = (left + right) >> 1;
-        if (index < mid)
+        return query(root, index, index + 1, 0, size);
+    }
+    NodeData query(int index_left, int index_right)
+    {
+        return query(root, index_left, index_right, 0, size);
+    }
+
+private:
+    int size;
+    TreeNode* root = new TreeNode();
+    NodeData build(V arr, TreeNode* node, int left, int right)
+    {
+        if (left + 1 == right)
         {
-            
+            node->data.sum = arr[left];
+            return node->data;
         }
+        int mid = (left + right) >> 1;
+        node->left = new TreeNode();
+        node->right = new TreeNode();
+        NodeData left_data = build(arr, node->left, left, mid);
+        NodeData right_data = build(arr, node->right, mid, right);
+        NodeData combined = NodeData::combine(left_data, right_data);
+        node->data = combined;
+        return combined;
+    }
+    NodeData query(TreeNode* node, int index_left, int index_right, int left, int right)
+    {
+        if (index_left == left && index_right == right)
+        {
+            return node->data;
+        }
+
+        int mid = (left + right) >> 1;
+        if (index_right <= mid)
+        {
+            return query(node->left, index_left, index_right, left, mid);
+        }
+        if (index_left >= mid)
+        {
+            return query(node->right, index_left, index_right, mid, right);
+        }
+
+        NodeData left_data = query(node->left, index_left, mid, left, mid);
+        NodeData right_data = query(node->right, mid, index_right, mid, right);
+        NodeData combined = NodeData::combine(left_data, right_data);
+        return combined;
     }
 };
 
@@ -66,14 +120,19 @@ void discrete(V &arr)
 
 void solve()
 {
-    cin >> n >> m;
-    arr.resize(n);
-    for (auto &i : arr)
-    {
-        cin >> i;
-    }
+    arr = {1, 2, 3, 4, 5, 6};
+    SegmentTree seg;
+    seg.build(arr);
+    cout << seg.query(1, 4).sum << nl;
 
-    discrete(arr);
+    // cin >> n >> m;
+    // arr.resize(n);
+    // for (auto &i : arr)
+    // {
+    //     cin >> i;
+    // }
+
+    // discrete(arr);
 
 }
 
