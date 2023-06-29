fbb82e4991129a41d905b912ab9b88d91659fd6f
WorldOfWheat <a302854888@gmail.com>
2023-06-30 02:41:03 +0800

    clean folder

--- CS/ConsoleApp2/Program.cs
+++ CS/ConsoleApp2/Program.cs
@@ -1,7 +1,14 @@
-﻿class Program
+﻿using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.Crypto.Generators;
+
+class Program
 {
     public static void Main(string[] args)
     {
-
+        SecureRandom random_engine = new SecureRandom();
+        BigInteger prime = BigInteger.ProbablePrime(7, random_engine);
+        Console.WriteLine(prime.ToString());
     }
 }
