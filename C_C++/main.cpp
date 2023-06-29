505817e1f0cdbe08532b5f775152a90b574a4e7b
WorldOfWheat <a302854888@gmail.com>
2023-06-30 00:40:08 +0800

    none

---
+++ C_C++/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define int long long
+#define V vector<int>
+#define VV vector<V>
+#define VP vector<pii>
+#define VVP vector<VP>
+#define PII pair<int, int>
+#define F first
+#define S second
+#define rep(x, y ,z) for(int x = y; x < z; x++)
+#define rep2(x, y ,z) for(int x = y; x <= z; x++)
+#define rrep(x, y ,z) for(int x = y; x >= z; x--)
+#define nl "\n"
+#define INF (int) 1e18
+#define MOD (int) 1e9 + 7
+
+using namespace std;
+
+void solve() 
+{
+
+}
+
+signed main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    cout.tie(0);
+
+	solve();
+
+	return 0;
+}
