645e6fbbace684359c8d5283cec4ff5f10a9300d
WorldOfWheat <a302854888@gmail.com>
2023-07-03 00:39:11 +0800

    algorithm practice

--- C_C++/main.cpp
+++ C_C++/main.cpp
@@ -5,14 +5,53 @@ typedef long long ll;
 using namespace std;
 
 int n;
+set<ll> se;
+set<ll> se2;
 
 void solve() 
 {
 	cin >> n;
-	for (ll i = 1; i <= n; i++)
+	ll sum = (1 + n) * n / 2;
+	if (sum % 2)
 	{
-		cout << ( ((i * i) * ((i * i) - 1)) / 2 ) - ( (2 * (i - 2) * (i - 1)) + (2 * (i - 2) * (i - 1)) ) << '\n';
+		cout << "NO" << '\n';
+		return;
 	}
+	cout << "YES" << '\n';
+	ll mid = sum / 2;
+	ll sum2 = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		se.insert(i);
+	}
+
+	ll diff = mid - sum2;
+	while (diff != 0)
+	{
+		auto lower = se.lower_bound(diff);
+		if (lower == se.end())
+		{
+			lower = prev(lower);
+		}
+		se2.insert(*lower);
+		se.erase(lower);
+		sum2 += *lower;
+		sum -= *lower;
+		diff = mid - sum2;
+	}
+
+	cout << (se.size()) << '\n';
+	for (auto i : se)
+	{
+		cout << i << ' ';
+	}
+	cout << '\n';
+	cout << (se2.size()) << '\n';
+	for (auto i : se2)
+	{
+		cout << i << ' ';
+	}
+	cout << '\n';
 }
 
 int main() 
