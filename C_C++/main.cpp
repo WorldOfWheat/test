4554dd10492e2b1cdb765ee5c70407ed2dc77553
WorldOfWheat <a302854888@gmail.com>
2023-07-17 15:03:41 +0800

    c++ test

--- C_C++/main.cpp
+++ C_C++/main.cpp
@@ -4,16 +4,13 @@ typedef long long ll;
 
 using namespace std;
 
+int n, m;
+vector<int> ve;
+
 void solve() 
 {
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			cout << "Taiwan No.1" << ' ';
-		}
-		cout << endl;
-	}
+	cout << setprecision(2) << 1.0 / 1.0;
+
 }
 
 int main() 
