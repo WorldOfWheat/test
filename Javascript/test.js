e6957d130661f2fa79cd46381fca8f7d19e7917c
WorldOfWheat <a302854888@gmail.com>
2023-07-20 23:47:52 +0800

    javascript practice

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,2 +1,10 @@
-var arr = ['test', 55, 5.1];
-console.log(arr);
+var test = {
+    "name": 5,
+    "name2": 6,
+    "name3": 7,
+}
+
+console.log(test);
+test.age = 5;
+delete test.name;
+console.log(test);
