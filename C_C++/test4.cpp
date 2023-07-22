72ef3c91cb61883566a88bee6e88f08c5f51ebe7
WorldOfWheat <a302854888@gmail.com>
2023-07-23 01:47:59 +0800

    Javascrpit practice

--- C_C++/test4.cpp
+++ C_C++/test4.cpp
@@ -2,33 +2,8 @@
 
 using namespace std;
 
-mt19937 mt;
-
-int get_random_seed()
-{
-    int* rand_seed_array = (int*) malloc(sizeof(int) * 100);
-    long long rand_seed = 0;
-    for (int i = 0; i < 100; i++)
-    {
-        rand_seed += *(rand_seed_array + i);
-    }
-
-    return rand_seed ^ time(0);
-}
-
-int get_random_between(int minimum, int maximum)
-{
-    return mt() % (maximum - minimum) + minimum;
-}
-
 int main()
 {
-    mt.seed(get_random_seed());
-
-    for (int i = 0; i < 32; i++)
-    {
-        cout << (get_random_between(0, 32));
-    }
 
     return 0;
 }
--- Javascript/test.js
+++
@@ -1,7 +0,0 @@
-// return x plus y
-const increment = function() {
-    return function increment(x, y) {
-        return x + y;
-    }
-}();
-console.log(increment(2, 10)) // Print: 1024
