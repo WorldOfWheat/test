59e268b898f0b4694ca919db1097b1563ca12869
WorldOfWheat <a302854888@gmail.com>
2023-11-08 15:01:52 +0800

    Add test

--- C_C++/test.cpp
+++ C_C++/test.cpp
@@ -4,29 +4,16 @@ using namespace std;
 
 typedef long long ll;
 
-int n;
+int n, m;
 vector<int> arr;
-vector<int> arr2;
 
 void solve()
 {
-    cin >> n;
-
+    cin >> n >> m;
+    
     arr.resize(n);
-
-    for (int i = 0; i < n; i++) cin >> arr[i];
-
-    for (int i = 0; i < n; i++)
-    {
-        auto upper = upper_bound(arr2.begin(), arr2.end(), arr[i]);
-        if (upper == arr2.end()) arr2.push_back(arr[i]);
-        else 
-        {
-            *upper = arr[i];
-        }
-    }
-
-    cout << (arr2.size()) << '\n';
+    arr.push_back()
+    
 }
 
 int main()
