9c39cc5c8bf344371e819c14db926f74da605bc9
WorldOfWheat <a302854888@gmail.com>
2023-12-01 14:12:15 +0800

    Add tests. Update tests

--- C_C++/test2.c
+++ C_C++/test2.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define N 80
 
 int main()
 {
-    int h,u,d;
-    scanf("%i %i %i",&h,&u,&d);
-    int sum=0;
-    if(u<=d)
-     printf("永遠無法到達");
-    else{
-     for(int i=1;sum<h;i++)
-     {
-      sum=sum+u;
-      if(sum>=h)
-      {
-        printf("%i",i);
-        break;
-      }
-      sum=sum-d;
-
-     }
-    }
+    
 }
--- Python/test.py
+++ Python/test.py
@@ -1,98 +1,27 @@
-from Crypto.Hash import SHA512, SHA1, MD5
-import os
-import sqlite3
+def enc(plain_text, key):
+    cipher_text = ''
+    for i in plain_text:
+        cipher_text += chr(((ord(i)-ord('a')) + key) % 26 + ord('a'))
+    return cipher_text
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.child = []
-        self.hashs = [3]
+def dec(cipher_text, key):
+    plain_text = ''
+    for i in cipher_text:
+        plain_text += chr(((ord(i)-ord('a')) - key) % 26 + ord('a'))
+    return plain_text
 
-    def add_child(self, child):
-        self.child.append(child)
-
-    def get_value(self):
-        return self.value
-
-def get_hash(data):
-    # data = data.encode()
-
-    md5 = MD5.new();
-    sha1 = SHA1.new();
-    sha512 = SHA512.new();
-
-    md5.update(data[0])
-    sha1.update(data[1])
-    sha512.update(data[2])
-
-    hash1 = md5.digest()
-    hash2 = sha1.digest()
-    hash3 = sha512.digest()
-    return (hash1, hash2, hash3)
-
-def get_all_paths(node):
-    current_path = node.get_value()
-
-    if (os.path.isfile(current_path)):
-        return node
-
-    dirs = os.listdir(current_path)
-    print(current_path, dirs)
-    if (len(dirs) == 0):
-        return node
-
-    for i in dirs:
-        child_path = os.path.join(current_path, i)
-        node.child.append(Node(child_path))
-        get_all_paths(node.child[-1])
-
-    return node
-
-def get_all_hashs(node):
-    if (len(node.child) == 0):
-        if (os.path.isdir(node.get_value())):
-            node.hashs = (b'', b'', b'')
-            return node.hashs
-        
-        with open(node.get_value(), 'rb') as f:
-            data = f.read()
-            node.hashs = get_hash((data, data, data))
-            return node.hashs
-    hashs = (b'', b'', b'') 
-    for i in node.child:
-        single_hash = get_all_hashs(i)
-        hashs = get_hash(
-            (
-            hashs[0] + single_hash[0], 
-            hashs[1] + single_hash[1], 
-            hashs[2] + single_hash[2]
-            )
-        )
-    node.hashs = hashs
-    return hashs
-
-def print_tree(node, count = 0):
-    print('\t' * count, end='')
-    if (len(node.child) == 0):
-        print(node.value, node.hashs[0].hex())
-        return
-    print(node.value, node.hashs[0].hex())
-    for i in node.child:
-        print_tree(i, count + 1)
-
-def main():
-    root = Node('.')
-
-    paths = get_all_paths(root)
-    # for i in paths.child:
-    #     print(i.get_value())
-    hashs = get_all_hashs(paths)
-    md5_hash = hashs[0].hex()
-    sh1_hash = hashs[1].hex()
-    sha512_hash = hashs[2].hex()
-    print(md5_hash, sh1_hash, sha512_hash)
-    print_tree(paths)
+def crack(cipher_text):
+    FREQ = 'etaoinshrdlcumwfgypbvkjxqz'
+    alphabet_count = [0] * 26
+    for i in range(26):
+        alphabet_count[i] = (cipher_text.count(chr(i+ord('a'))), i)
+    alphabet_count.sort(reverse=True)
+    # print(alphabet_count)
 
 
-if __name__ == '__main__':
-    main()
+
+data = 'chinese'
+cipher_text = enc(data, 3)
+print(cipher_text)
+print(dec(cipher_text, 3))
+crack(cipher_text)
