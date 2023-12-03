be7fc8ad9583d0879daf6a386269f3a47b1364d3
WorldOfWheat <a302854888@gmail.com>
2023-12-03 09:38:24 +0800

    Add new solves

--- C_C++/test.cpp
+++ C_C++/test.cpp
@@ -5,41 +5,10 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int n, m;
-vector<int> arr;
 
 void solve()
 {
-    cin >> n >> m;
-    
-    arr.resize(n);
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    
-    int ans = 0;
-    int l = 0, r = 0;
-    int sum = arr[r];
-    while (r < n)
-    {
-        if (sum == m) 
-        {
-            ans++;
-            r++;
-            sum += arr[r];
-        }
-        // cerr << l << ' ' << r << ' ' << sum << '\n';
-        while (sum < m) 
-        {
-            r++;
-            sum += arr[r];
-        }
-        while (sum > m) 
-        {
-            sum -= arr[l];
-            l++;
-        }
-    }
-    
-    cout << ans << '\n';
+
 }
 
 int main()
