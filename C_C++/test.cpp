4fe0ec22c1580212a06d40938b617639c316b8c1
WorldOfWheat <a302854888@gmail.com>
2023-12-03 09:39:21 +0800

    Tests
    	modified:   C_C++/test.c

--- C_C++/test.cpp
+++ C_C++/test.cpp
@@ -8,14 +8,17 @@ typedef pair<int, int> pii;
 
 void solve()
 {
-
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    do {
+        for (int i = 0; i < arr.size(); i++) cout << arr[i] << endl;
+    } while (next_permutation(arr.begin(), arr.end()));
 }
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+    // cout.tie(0);
 
     solve();
 
