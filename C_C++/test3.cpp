c87fb4050e196df64cff34f0546378a8a98b3286
WorldOfWheat <a302854888@gmail.com>
2023-12-05 14:04:54 +0800

    Update tests

--- C_C++/test3.cpp
+++ C_C++/test3.cpp
@@ -1,13 +1,57 @@
 #include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio; cin.tie(0); cout.tie(0)
+#define debug(container) for (auto i : container) cerr << i << ' '; cerr << '\n';
+#define ll long long;
+#define pii pair<int, int>;
 
 using namespace std;
 
-typedef long long ll;
+int n, m;
+vector<int> arr;
+vector<int> visited;
+
+void coord_compress(vector<int> &arr) 
+{
+    vector<int> mapping(arr);
+    sort(mapping.begin(), mapping.end());
+    mapping.erase(unique(mapping.begin(), mapping.end()));
+    
+    for (int i = 0; i < arr.size(); i++)
+    {
+        auto lower = lower_bound(begin(mapping), end(mapping), arr[i]);
+        int index = distance(begin(mapping), lower);
+        arr[i] = index;        
+    }
+}
 
 void solve()
 {
+    cin >> n >> m;
+    
+    arr.resize(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    
+    if (n == 1)
+    {
+        cout << 1 << '\n';
+        return;
+    }
 
+    coord_compress(arr);
+    
+    visited.resize(2e5 + 5);
+    int ans = 0;
+    int l = 0, r = 0;
+    visited[arr[l]]++;
+    while (r < n) 
+    {
+        while (arr[r] < 1) 
+        {
+        }
+        while ()
+    }
+    
+    cout << ans << '\n';
 }
 
 int main()
