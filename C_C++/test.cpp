69da0299703251c0d95ac2acfd8b40d81d4ec15a
WorldOfWheat <a302854888@gmail.com>
2024-01-17 14:41:38 +0800

    Update tests

--- C_C++/test.cpp
+++ C_C++/test.cpp
@@ -14,12 +14,12 @@ typedef vector<string> vs;
 
 v2i matrix_multiply(v2i matrix_a, v2i matrix_b)
 {
-    v2i result(matrix_b.size());
-
     int size_a_row = matrix_a.size();
     int size_b_row = matrix_b.size();
     int size_a_column = matrix_a[0].size();
     int size_b_column = matrix_b[0].size();
+    
+    v2i result(size_a_row, vi(size_a_column));
 
     int result_x = 0, result_y = 0;
     for (int i = 0; i < size_a_row; i++) 
@@ -35,22 +35,74 @@ v2i matrix_multiply(v2i matrix_a, v2i matrix_b)
     return result;
 }
 
+void print_matrix(v2i matrix)
+{
+    int size_row = matrix.size();
+    int size_column = matrix[0].size();
+    for (int i = 0; i < size_row; i++)
+    {
+        for (int j = 0; j < size_column; j++)
+        {
+            cout << matrix[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+v2i matrix_fast_pow(v2i matrix, int power, int mod = 1e9 + 7)
+{
+    v2i result = {
+        {1, 1, 1},
+        {1, 0, 0},
+        {0, 1, 0},
+    };
+
+    v2i current_value(matrix);
+    while (power > 0)    
+    {
+        if (power & 1) 
+        {
+            result = matrix_multiply(current_value, result);
+        }
+        power >>= 1;
+        current_value = matrix_multiply(current_value, current_value);
+    }
+
+    return result;
+}
+
+int n;
+vi arr(3);
+
 void solve()
 {
-    v2i matrix_a = 
+    cin >> n;
+
+    for (int i = 0; i < 3; i++) cin >> arr[i];
+    
+    if (n <= 3) 
     {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
-    };
-    v2i matrix_b = 
-    {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
+        cout << arr[n-1] << ' ';
+        return;
+    }
+
+    const v2i matrix = {
+        {1, 1, 1},
+        {1, 0, 0},
+        {0, 1, 0},
     };
     
-    v2i result = matrix_multiply(matrix_a, matrix_b);
+    const v2i multiplier =
+    {
+        {arr[0]},
+        {arr[1]},
+        {arr[2]},
+    };
+
+    const v2i multiplicand = matrix_fast_pow(matrix, n - 3 - 1);
+
+    int result = matrix_multiply(multiplicand, multiplier)[0][0];
+    cout << result << '\n';
 }
 
 signed main() 
